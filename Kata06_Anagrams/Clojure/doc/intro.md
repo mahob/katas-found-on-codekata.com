0ed8c228af6fadf7fee30c7269fd7861f16d266f
mahob <iammahob@gmail.com>
2014-12-21 22:24:29 +0100

    Removed messed up implementation of Kata06 in Clojure

--- Kata06_Anagrams/Clojure/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to kata06_anagrams_clojure
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- Kata06_Anagrams/Clojure/src/kata06_anagrams_clojure/core.clj
+++
@@ -1,9 +0,0 @@
-(ns kata06-anagrams-clojure.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (with-open [r (clojure.java.io/reader "stuff.txt")]
-   (doseq [line (line-seq r)]
-    (println line))))
