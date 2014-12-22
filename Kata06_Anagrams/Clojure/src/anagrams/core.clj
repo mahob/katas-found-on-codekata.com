8260e8da866799b98fa08b28b359661cb67fbb5b
mahob <iammahob@gmail.com>
2014-12-22 22:58:48 +0100

    Managed to run and understand the char-counter function.

--- Kata06_Anagrams/Clojure/src/anagrams/core.clj
+++ Kata06_Anagrams/Clojure/src/anagrams/core.clj
@@ -6,14 +6,21 @@
   [word]
   (apply str (sort word)))
 
-(defn add-anagram
-  "Adds an anagram to the table of anagrams."
-  [anagram]
-  (1))
+(defn char-counter 
+  "Counts characters in a string."
+  [str]
+  (loop [counts {}
+         s str]
+    (if-not (empty? s)
+      (let [c (first s)]
+        (recur (assoc counts c (inc (get counts c 0)))
+               (rest s)))
+      counts)))
 
 (defn -main
   "Builds and counts anagrams of the words in the wordlist.txt file."
   [& args]
-  (with-open [r (clojure.java.io/reader "../wordlist.txt")]
-    (doseq [line (line-seq r)]
-      (println line (build-anagram line)))))
+  (println (clojure.string/split-lines (slurp "../wordlist.txt"))))
+;  (with-open [r (clojure.java.io/reader "../wordlist.txt")]
+;    (doseq [line (line-seq r)]
+;      (println line (build-anagram line)))))
--- Kata06_Anagrams/Clojure/test/anagrams/core_test.clj
+++ Kata06_Anagrams/Clojure/test/anagrams/core_test.clj
@@ -6,6 +6,14 @@
   (testing "anagrams.core build-anagram"
      (is (= (build-anagram "hello") "ehllo"))))
 
-(deftest add-anagram-test
-  (testing "anagrams.core add-anagram"
-     (is (= (add-anagram "ehllo") 1))))
+;(deftest add-anagram-test
+;  (testing "anagrams.core add-anagram"
+ ;    (is (= (add-anagram "ehllo") 1))))
+
+;(deftest anagram-counter-test
+;  (testing "basic anagram conting"
+;    (is (= (anagram-counter "ab\nba") {"ab" ["ab" "ba"]}))))
+
+(deftest char-counter-test
+  (testing "counting chars"
+           (is (= (char-counter "bab") {\a 1, \b 2}))))
