bce5164672f008d64a982482a1256a7adae2fc07
mahob <iammahob@gmail.com>
2014-12-27 17:35:06 +0100

    Added some comments and improved the output

--- Kata06_Anagrams/Clojure/src/anagrams/core.clj
+++ Kata06_Anagrams/Clojure/src/anagrams/core.clj
@@ -7,12 +7,12 @@
   (apply str (sort word)))
 
 (defn file-lines
-  "Splits all lines of with thegiven filename"
+  "Splits all lines of the file with the given filename"
   [filename]
   (clojure.string/split-lines (slurp filename)))
 
 (defn anagrams-from-file
-  ""
+  "Builds anagrams from all words in the file with the given fiename."
   [filename]
   (loop [anagrams {}
          wordlist (file-lines filename)]
@@ -26,4 +26,5 @@
 (defn -main
   "Builds and counts anagrams of the words in the wordlist.txt file."
   [& args]
-  (println (anagrams-from-file "test/resources/medium-wordlist.txt")))
+  (doseq [[k v] (anagrams-from-file "test/resources/medium-wordlist.txt")]
+    (println v)))
