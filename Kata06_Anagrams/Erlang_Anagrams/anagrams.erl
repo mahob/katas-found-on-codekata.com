91f84326e066b9b0f7289e5cacf7707a9634f4cf
mahob <iammahob@gmail.com>
2015-02-25 23:16:15 +0100

    Erlang anagrams kata fails when using the full wordlist.txt

--- Kata06_Anagrams/Erlang_Anagrams/anagrams.erl
+++ Kata06_Anagrams/Erlang_Anagrams/anagrams.erl
@@ -1,22 +1,32 @@
 -module(anagrams).
 -export([read_wordlist/1]).
+-import(lists, [delete/2, sort/2]).
 
 read_wordlist(FileName) ->
   Dict = for_each_line_in_file(FileName, [read, {encoding, utf8}]),
-  Anagrams = dict:to_list(Dict),
-  [io:format("~n" ++ Key ++ ": " ++ string:join(Values, ", ")) || {Key, Values} <- Anagrams].
+  Anagrams = [Values || {_, Values} <- dict:to_list(Dict)],
+  Sorted = sort(fun(A, B) -> length(A) >= length(B) end, clean(Anagrams)),
+  [io:format(string:join(L, ", ") ++ "~n") || L <- Sorted].
+
+clean(List) ->
+  clean(List, List).
+
+clean([H|T], Keep) ->
+  case length(H) =< 1 of
+    true  -> clean(T, delete(H, Keep));
+    false -> clean(T, Keep)
+  end;
+clean([], Keep) ->
+  Keep.
 
 for_each_line_in_file(Name, Mode) ->
   {ok, Device} = file:open(Name, Mode),
-  for_each_line(Device).
+  for_each_line(Device, dict:new(), 0).
 
 % Remove leading and trailing whitespaces
 % (string:split doesn't remove readling/trailing line feeds)
 strip(Text) ->
   re:replace(Text, "(^\\s+)|(\\s+$)", "", [global,{return,list}]).
-
-for_each_line(Device) ->
-  for_each_line(Device, dict:new(), 0).
 
 for_each_line(Device, Dict, LineNumber) ->
   case io:get_line(Device, "") of
@@ -26,7 +36,6 @@ for_each_line(Device, Dict, LineNumber) ->
     Data ->
       Line = strip(Data),
       Anagram = lists:sort(Line),
-      %           io:format("~n---~nLine [" ++ integer_to_list(LineNumber) ++"] " ++ Line ++ "~nAnagram: " ++ Anagram),
       NewDict = dict:update(Anagram, fun(Old) -> Old ++ [Line] end, [Line], Dict),
       for_each_line(Device, NewDict, LineNumber + 1)
   end.
